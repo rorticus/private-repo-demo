4632229991d5fad4de841e8a9f198c0bcfc42eb2
Rory Mulligan <rmulligan@sitepen.com>
2023-01-10 12:55:00 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { sharedBusinessLogic } from '@rorticus/private-repo';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>The value returned is {sharedBusinessLogic()}</Text>
       <StatusBar style="auto" />
     </View>
   );
